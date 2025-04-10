176f8afb5130aeae5fe9fb197201d91cbd4b690c
Braian <braian_019@hotmail.com>
2025-04-10 14:31:21 +0200

    Json-server and axios library were added, new fields in form were added, and connection with the test database is established, clients view display was added

--- src/views/AddClientView.vue
+++ src/views/AddClientView.vue
@@ -1,13 +1,19 @@
 <script setup>
+import { reactive } from 'vue'
 import RouterLink from '@/components/UI/RouterLink.vue'
 import Heading from '@/components/UI/Heading.vue'
 import { FormKit } from '@formkit/vue'
 
 defineProps({
   title: {
-    type: String
-  }
+    type: String,
+  },
 })
+
+const handleSubmit = (data)=>{
+  console.log(data)
+}
+
 </script>
 
 <template>
@@ -20,32 +26,45 @@ defineProps({
 
     <div class="mx-auto mt-10 bg-white shadow">
       <div class="mx-auto md:w-2/3 py-20 px-6">
+
         <FormKit type="form"
-        >
+                 submit-label="Add Client"
+                 @submit="handleSubmit">
+
           <FormKit type="text"
                    label="Name"
+                   name="name"
                    placeholder="Client Name"
-                   validation="required" />
-          <FormKit type="text"
-                   label="Last Name"
-                   placeholder="Client Last Name"
-                   validation="required" />
-          <FormKit type="email"
-                   label="E-mail"
-                   placeholder="Client E-mail"
-                   validation="required|email" />
-          <FormKit type="text"
-                   label="Phone Number"
-                   placeholder="Phone Number: XXX XXXX XXXX"
-                   validation="*matches:/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/"
-                   :validation-messages="{matches:`Invalid number, use ''-'' to separate the number`}" />
-
+                   validation="required"
+          />
+          <FormKit
+            type="text"
+            label="Last Name"
+            name="lastName"
+            placeholder="Client Last Name"
+            validation="required"
+          />
+          <FormKit
+            type="email"
+            label="E-mail"
+            name="email"
+            placeholder="Client E-mail"
+            validation="required|email"
+          />
+          <FormKit
+            type="text"
+            label="Phone Number"
+            name="phoneNumber"
+            placeholder="Phone Number: XXX XXXX XXXX"
+            validation="*matches:/^[0-9]{12}$/"
+            :validation-messages="{ matches: `Invalid number, use ''-'' to separate the number` }"
+          />
+          <FormKit type="text" label="Company" name="company" placeholder="Client Company" />
+          <FormKit type="text" label="Position" name="position" placeholder="Client Company Position" />
         </FormKit>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { computed, onMounted, ref } from 'vue'
+import axios from 'axios'
 import RouterLink from '@/components/UI/RouterLink.vue'
 import Heading from '@/components/UI/Heading.vue'
+
+const clients = ref([])
+
+onMounted(() => {
+  axios('http://localhost:4000/clients') //Get is the default method so it can be omitted
+    .then(({data}) => clients.value = data)
+    .catch((error) => console.log('Not Data Found', error))
+
+})
 
 defineProps({
   title: {
     type: String,
   }
 })
+
+const clientsExistence = computed(() => clients.value.length > 0)
 </script>
 
 <template>
@@ -17,5 +30,27 @@ defineProps({
       </RouterLink>
     </div>
     <Heading>{{ title }}</Heading>
+
+    <div v-if="clientsExistence" class="flow-root mx-auto  mt-10 p-5 bg-white shadow">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+            <tr>
+              <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Name</th>
+              <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Company</th>
+              <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">State</th>
+              <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Actions</th>
+            </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <p v-else class="text-center mt-10">There are no clients yet</p>
   </div>
 </template>
